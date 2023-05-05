5409bf42e3adbf4d11d25918b5b16255ebecb21d
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2023-05-04 22:29:42 -0300

    ainda não ta pronto

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const trocou = document.getElementById("encherCopo")
-trocou.addEventListener('click', () => alert("apertou"))
+const trocou = () => {
+    trocou.src = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjAX6V1A4DTaP0n-XeZQ0bTHLn8ey9jx7_NipWE_Uz&s`
+}
+
+const trocar = () => {
+    const  imagem = document.getElementById("encherCopo")
+    imagem.addEventListener("click",trocou )
+}
